85b1b5e056a578eb9d75a3741bc265f1ccef893e
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-06 16:47:30 +0330

    feat: create ThemeProvider

---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+import { themes } from "../utils/constants";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const currentTheme = localStorage.getItem("theme") || themes.DARK;
+    return currentTheme;
+  });
+
+  const setThemeDark = () => {
+    setTheme(themes.DARK);
+    localStorage.setItem("theme", themes.DARK);
+  };
+
+  const setThemeLight = () => {
+    setTheme(themes.LIGHT);
+    localStorage.setItem("theme", themes.LIGHT);
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setThemeDark,
+        setThemeLight,
+      }}
+    >
+      <div className="App" data-theme={theme}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
