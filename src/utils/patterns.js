c190099a3187a610a9e35718c578be7bfd50ce75
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-06 18:33:47 +0330

    feat: add usernamePattern

--- src/utils/patterns.js
+++ src/utils/patterns.js
@@ -1,3 +1,4 @@
 const emailPattern = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
+const usernamePattern = /^[a-z]{1}[a-z0-9]{2,}(_[a-z0-9]+)*?$/;
 
-export { emailPattern };
+export { emailPattern, usernamePattern };
