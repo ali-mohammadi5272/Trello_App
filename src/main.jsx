c58ed69ae32ea11d3d0d623ccf090fd99d4bd82c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-06 16:48:26 +0330

    feat: add ThemeProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./Index.scss";
+import ThemeProvider from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
