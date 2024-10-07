335a8c8f05a8ed8dd925444bea7f6dec9d833977
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 18:15:41 +0330

    feat: add IndexPage route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,16 @@
 import LoginPage from "./components/templates/LoginPage/LoginPage";
 import HomePage from "./components/templates/HomePage/HomePage";
 import RegisterPage from "./components/templates/RegisterPage/RegisterPage";
+import IndexPage from "./components/templates/IndexPage/IndexPage";
 
 const routes = [
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
-  { path: "/", element: <HomePage /> },
+  {
+    path: "/",
+    element: <IndexPage />,
+    children: [],
+  },
 ];
 
 export { routes };
