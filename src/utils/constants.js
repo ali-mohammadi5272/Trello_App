d18e1da831b7883039cd027079c45bcf4ee16796
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:36:36 +0330

    feat: add yupMessages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,4 +16,42 @@ const authMessages = {
   },
 };
 
-export { themes, dbDefaultTables, authMessages };
+const yupMessages = {
+  login: {
+    identifier: {
+      min: "Username/Email must be at least 3 characters !!",
+      required: "Username/Email is required !!",
+    },
+    password: {
+      min: "Password must be at least 8 characters !!",
+      required: "Password is required !!",
+    },
+  },
+  register: {
+    firstname: {
+      min: "firstname must be at least 3 characters !!",
+      required: "firstname is required !!",
+    },
+    lastname: {
+      min: "lastname must be at least 3 characters !!",
+      required: "lastname is required !!",
+    },
+    username: {
+      min: "username must be at least 3 characters !!",
+      required: "username is required !!",
+      pattern:
+        "Username is not valid. Valid values are: john or john_doe or john_doe_john",
+    },
+    email: {
+      min: "Email is not valid !!",
+      required: "email is required !!",
+      pattern: "Email is not valid !!",
+    },
+    password: {
+      min: "password must be at least 8 characters !!",
+      required: "password is required !!",
+    },
+  },
+};
+
+export { themes, dbDefaultTables, authMessages, yupMessages };
