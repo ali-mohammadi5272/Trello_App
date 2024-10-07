05de40ae15caff14c31d3354ae035aa02c3af03d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 23:08:55 +0330

    feat: create ColumnAndTaskCreator Component

---
+++ src/components/modules/ColumnAndTaskCreator/ColumnAndTaskCreator.jsx
@@ -0,0 +1,25 @@
+import { isFormEmpty } from "../../../utils/helperFunctions";
+import FontAwesomeIcon from "../FontawesomeIcon/FontAwesomeIcon";
+import styles from "./ColumnAndTaskCreator.module.scss";
+
+const ColumnAndTaskCreator = ({ onSubmit, onChange, data }) => {
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
+      <input
+        value={data.value}
+        onChange={onChange}
+        type="text"
+        className={styles.form__input}
+      />
+      <button
+        disabled={isFormEmpty(data)}
+        type="submit"
+        className={styles.form__btn}
+      >
+        <FontAwesomeIcon icon="faPlus" />
+      </button>
+    </form>
+  );
+};
+
+export default ColumnAndTaskCreator;
