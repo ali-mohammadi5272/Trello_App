fdc9e01cc7c1239854f4e1b1e75933efaa87fffe
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:37:20 +0330

    feat: create register validator

---
+++ src/utils/validators/register.js
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+import { yupMessages } from "../constants";
+import { usernamePattern } from "../patterns";
+
+const { email, firstname, lastname, password, username } = yupMessages.register;
+
+const registerSchema = Yup.object().shape({
+  firstname: Yup.string().min(3, firstname.min).required(firstname.required),
+
+  lastname: Yup.string().min(3, lastname.min).required(lastname.required),
+
+  username: Yup.string()
+    .min(3, username.min)
+    .required(username.required)
+    .matches(usernamePattern),
+
+  email: Yup.string()
+    .min(5, email.min)
+    .email(email.pattern)
+    .required(email.required),
+
+  password: Yup.string().min(8, password.min).required(password.required),
+});
+
+export default registerSchema;
