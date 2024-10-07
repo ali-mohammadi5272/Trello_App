8539f34e7092a9f22d94ae29cc22e6fca45e1c8d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:31:09 +0330

    feat: add main.scss styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./context/ThemeProvider.jsx";
 import "react-toastify/dist/ReactToastify.css";
+import "./assets/styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
