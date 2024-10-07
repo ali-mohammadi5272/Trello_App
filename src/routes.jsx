4273943f37bb9843af7f5fe557137f3e1a7a0889
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 18:23:55 +0330

    feat: add AuthProvider

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,18 @@ import LoginPage from "./components/templates/LoginPage/LoginPage";
 import HomePage from "./components/templates/HomePage/HomePage";
 import RegisterPage from "./components/templates/RegisterPage/RegisterPage";
 import IndexPage from "./components/templates/IndexPage/IndexPage";
+import AuthProvider from "./context/AuthProvider";
 
 const routes = [
   { path: "/login", element: <LoginPage /> },
   { path: "/register", element: <RegisterPage /> },
   {
     path: "/",
-    element: <IndexPage />,
+    element: (
+      <AuthProvider>
+        <IndexPage />
+      </AuthProvider>
+    ),
     children: [],
   },
 ];
