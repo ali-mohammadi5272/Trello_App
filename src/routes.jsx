c129956e5ed394d4e8c38419aee34a37cedb1d16
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:35:22 +0330

    refactor: change messages to authMessages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,12 @@
-const routes = [];
+import LoginPage from "./components/templates/LoginPage/LoginPage";
+import HomePage from "./components/templates/HomePage/HomePage";
+import SignUpPage from "./components/templates/SignUpPage/SignUpPage";
+import RegisterPaaagae from "./components/templates/RegisterPaaaage/RegisterPaaaage";
+
+const routes = [
+  { path: "/login", element: <LoginPage /> },
+  { path: "/register", element: <RegisterPaaagae /> },
+  { path: "/", element: <HomePage /> },
+];
 
 export { routes };
