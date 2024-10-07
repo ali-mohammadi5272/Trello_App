e8ea63dfd70fef7a7ee96fba045c8045394306dc
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 16:04:57 +0330

    feat: add RegisterPage route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,10 @@
 import LoginPage from "./components/templates/LoginPage/LoginPage";
 import HomePage from "./components/templates/HomePage/HomePage";
-import SignUpPage from "./components/templates/SignUpPage/SignUpPage";
-import RegisterPaaagae from "./components/templates/RegisterPaaaage/RegisterPaaaage";
+import RegisterPage from "./components/templates/RegisterPage/RegisterPage";
 
 const routes = [
   { path: "/login", element: <LoginPage /> },
-  { path: "/register", element: <RegisterPaaagae /> },
+  { path: "/register", element: <RegisterPage /> },
   { path: "/", element: <HomePage /> },
 ];
 
