67c0ab52f2dc7bfcd5d9b8188780847df100599d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:30:46 +0330

    feat: add react-toastify styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./context/ThemeProvider.jsx";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
