c66d0ffaf783e9ad368ea78a47012cfe85680d79
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-07 15:30:16 +0330

    refactor: remove Index.scss import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "./Index.scss";
 import ThemeProvider from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
