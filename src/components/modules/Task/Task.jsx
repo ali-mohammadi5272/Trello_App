be120312e01040520fae3b657d3b2cfcc58e72b2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-08 18:11:00 +0330

    feat: create Task component

--- src/components/modules/Task/Task.jsx
+++ src/components/modules/Task/Task.jsx
@@ -1,9 +1,14 @@
 import styles from "./Task.module.scss";
 
 const Task = ({ id, title, columnId }) => {
+  const dragStartHandler = (e) => {
+    const task = JSON.stringify({ id, title, columnId });
+    e.dataTransfer.setData("string", task);
+  };
+
   return (
     <div className={styles.taskContainer}>
-      <div className={styles.task} draggable>
+      <div className={styles.task} draggable onDragStart={dragStartHandler}>
         <h3 className={styles.task__title}>{title}</h3>
       </div>
     </div>
