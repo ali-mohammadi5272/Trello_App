022365c5d91e4be8b5e17e58fea30c93ef90e064
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-08 22:39:38 +0330

    feat: add OffCanvas Component

--- src/components/modules/Navbar/Navbar.jsx
+++ src/components/modules/Navbar/Navbar.jsx
@@ -5,11 +5,14 @@ import styles from "./Navbar.module.scss";
 import { removeCookie } from "../../../utils/helperFunctions";
 import swal from "sweetalert";
 import { AuthContext } from "../../../context/AuthProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import OffCanvasMenu from "../OffCanvasMenu/OffCanvasMenu";
+import FontAwesomeIcon from "../FontawesomeIcon/FontAwesomeIcon";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { setUser, user } = useContext(AuthContext);
+  const [showOfCanvas, setShowOfCanvas] = useState(false);
 
   const logOutHandler = () => {
     swal({
@@ -26,30 +29,70 @@ const Navbar = () => {
     });
   };
 
+  const offCanvasMenuHnadler = () => {
+    setShowOfCanvas((prev) => !prev);
+  };
+
   return (
-    <div className={styles.navbarContainer}>
-      <Container>
-        <nav className={`${styles.navbarContainer__navbar} ${styles.navbar}`}>
-          <section className={styles.navbar__section}>
-            <ToggleThemeBtn className={styles.navbar__togglThemeBtn} />
-          </section>
-          <section className={styles.navbar__section}>
+    <>
+      <div className={styles.navbarContainer}>
+        <Container>
+          <nav className={`${styles.navbarContainer__navbar} ${styles.navbar}`}>
+            <section className={styles.navbar__section}>
+              <ToggleThemeBtn className={styles.navbar__togglThemeBtn} />
+            </section>
+            <section className={styles.navbar__section}>
+              {user && (
+                <h3 className={styles.navbar__user}>
+                  {user.firstname} {user.lastname}
+                </h3>
+              )}
+              <button
+                onClick={logOutHandler}
+                className={styles.navbar__logoutBtn}
+                to="/login"
+              >
+                Sign Out
+              </button>
+              <button
+                onClick={offCanvasMenuHnadler}
+                className={styles.navbar__offCanvasBtn}
+              >
+                <FontAwesomeIcon icon="faBars" />
+              </button>
+            </section>
+          </nav>
+        </Container>
+      </div>
+      <OffCanvasMenu
+        show={showOfCanvas}
+        offCanvasBtnClick={offCanvasMenuHnadler}
+        className={styles.offCanvasMenuContainer}
+      >
+        <div className={styles.offCanvasMenu}>
+          <header className={styles.offCanvasMenu__header}>
             {user && (
-              <h3 className={styles.navbar__user}>
+              <h3 className={styles.offCanvasMenu__user}>
                 {user.firstname} {user.lastname}
               </h3>
             )}
             <button
-              onClick={logOutHandler}
-              className={styles.navbar__logoutBtn}
-              to="/login"
+              onClick={offCanvasMenuHnadler}
+              className={styles.navbar__offCanvasBtn}
             >
-              Sign Out
+              <FontAwesomeIcon icon="faXmark" />
             </button>
-          </section>
-        </nav>
-      </Container>
-    </div>
+          </header>
+          <button
+            onClick={logOutHandler}
+            className={styles.offCanvasMenu__logoutBtn}
+            to="/login"
+          >
+            Sign Out
+          </button>
+        </div>
+      </OffCanvasMenu>
+    </>
   );
 };
 
