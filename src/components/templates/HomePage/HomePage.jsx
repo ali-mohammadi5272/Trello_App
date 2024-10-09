f2ceb6abfff0a354eb69848f474f20eb4da7c815
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-10-09 09:08:11 +0330

    feat: HomePage Title

--- src/components/templates/HomePage/HomePage.jsx
+++ src/components/templates/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
 import Container from "../../modules/Container/Container";
 import styles from "./HomePage.module.scss";
 import Columns from "../../modules/Columns/Columns";
+import { useLayoutEffect } from "react";
 
 const HomePage = () => {
+  useLayoutEffect(() => {
+    document.title = "Home";
+  }, []);
+
   return (
     <main className={styles.page}>
       <Container>
